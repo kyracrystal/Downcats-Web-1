90d1ff372d44eeffb57fc277e0c7b3eed314c2e6
Kyra <kyradatemasch@pm.me>
2024-09-03 23:38:27 -0400

    Made logo smaller

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -6,7 +6,7 @@ export const HeaderLogo = () => {
         <Link href='/'>
             <div className="items-center hidden lg:flex">
                 <Image src='/downcat-light.png' alt='Logo' height={40} width={40}/>
-                <p className="font-semibold text-[#ffffff] text-3xl ml-2.5">
+                <p className="font-semibold text-[#ffffff] text-2xl ml-2.5">
                     Downcats Dev
                 </p>
             </div>
